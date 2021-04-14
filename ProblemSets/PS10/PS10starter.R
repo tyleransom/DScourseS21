ef6742907fc7faa74c9ca4fbaf5ae042ce2935cc
Tyler Ransom <tyleransom@gmail.com>
2021-04-14 17:23:59 -0500

    update PS10

--- ProblemSets/PS10/PS10starter.R
+++ ProblemSets/PS10/PS10starter.R
@@ -1,6 +1,15 @@
+library(tidyverse)
+library(tidymodels)
+library(magrittr)
+library(rpart)
+library(e1071)
+library(kknn)
+library(nnet)
+library(kernlab)
+
 set.seed(100)
 
-income <- read.table("http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data")
+income <- read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data", col_names = FALSE)
 names(income) <- c("age","workclass","fnlwgt","education","education.num","marital.status","occupation","relationship","race","sex","capital.gain","capital.loss","hours","native.country","high.earner")
 
 # From UC Irvine's website (http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.names)
@@ -23,26 +32,200 @@ names(income) <- c("age","workclass","fnlwgt","education","education.num","marit
 # Clean up the data
 ######################
 # Drop unnecessary columns
-income$native.country <- NULL
-income$fnlwgt         <- NULL
-# Make sure continuous variables are coded as such
-income$age            <- as.numeric(income$age)
-income$hours          <- as.numeric(income$hours)
-income$education.num  <- as.numeric(income$education.num)
-income$capital.gain   <- as.numeric(income$capital.gain)
-income$capital.loss   <- as.numeric(income$capital.loss)
-# Combine levels of categorical variables that currently have too many levels
-levels(income$education) <- list(Advanced = c("Masters,","Doctorate,","Prof-school,"), Bachelors = c("Bachelors,"), "Some-college" = c("Some-college,","Assoc-acdm,","Assoc-voc,"), "HS-grad" = c("HS-grad,","12th,"), "HS-drop" = c("11th,","9th,","7th-8th,","1st-4th,","10th,","5th-6th,","Preschool,"))
-levels(income$marital.status) <- list(Married = c("Married-civ-spouse,","Married-spouse-absent,","Married-AF-spouse,"), Divorced = c("Divorced,","Separated,"), Widowed = c("Widowed,"), "Never-married" = c("Never-married,"))
-levels(income$race) <- list(White = c("White,"), Black = c("Black,"), Asian = c("Asian-Pac-Islander,"), Other = c("Other,","Amer-Indian-Eskimo,"))
-levels(income$workclass) <- list(Private = c("Private,"), "Self-emp" = c("Self-emp-not-inc,","Self-emp-inc,"), Gov = c("Federal-gov,","Local-gov,","State-gov,"), Other = c("Without-pay,","Never-worked,","?,"))
-levels(income$occupation) <- list("Blue-collar" = c("?,","Craft-repair,","Farming-fishing,","Handlers-cleaners,","Machine-op-inspct,","Transport-moving,"), "White-collar" = c("Adm-clerical,","Exec-managerial,","Prof-specialty,","Sales,","Tech-support,"), Services = c("Armed-Forces,","Other-service,","Priv-house-serv,","Protective-serv,"))
+income %<>% select(-native.country, -fnlwgt, education.num)
+# Make sure continuous variables are formatted as numeric
+income %<>% mutate(across(c(age,hours,education.num,capital.gain,capital.loss), as.numeric))
+# Make sure discrete variables are formatted as factors
+income %<>% mutate(across(c(high.earner,education,marital.status,race,workclass,occupation,relationship,sex), as.factor))
+# Combine levels of factor variables that currently have too many levels
+income %<>% mutate(education = fct_collapse(education,
+                                            Advanced    = c("Masters","Doctorate","Prof-school"), 
+                                            Bachelors   = c("Bachelors"), 
+                                            SomeCollege = c("Some-college","Assoc-acdm","Assoc-voc"),
+                                            HSgrad      = c("HS-grad","12th"),
+                                            HSdrop      = c("11th","9th","7th-8th","1st-4th","10th","5th-6th","Preschool") 
+                                           ),
+                   marital.status = fct_collapse(marital.status,
+                                            Married      = c("Married-civ-spouse","Married-spouse-absent","Married-AF-spouse"), 
+                                            Divorced     = c("Divorced","Separated"), 
+                                            Widowed      = c("Widowed"), 
+                                            NeverMarried = c("Never-married")
+                                           ), 
+                   race = fct_collapse(race,
+                                            White = c("White"), 
+                                            Black = c("Black"), 
+                                            Asian = c("Asian-Pac-Islander"), 
+                                            Other = c("Other","Amer-Indian-Eskimo")
+                                           ), 
+                   workclass = fct_collapse(workclass,
+                                            Private = c("Private"), 
+                                            SelfEmp = c("Self-emp-not-inc","Self-emp-inc"), 
+                                            Gov     = c("Federal-gov","Local-gov","State-gov"), 
+                                            Other   = c("Without-pay","Never-worked","?")
+                                           ), 
+                   occupation = fct_collapse(occupation,
+                                            BlueCollar  = c("?","Craft-repair","Farming-fishing","Handlers-cleaners","Machine-op-inspct","Transport-moving"), 
+                                            WhiteCollar = c("Adm-clerical","Exec-managerial","Prof-specialty","Sales","Tech-support"), 
+                                            Services    = c("Armed-Forces","Other-service","Priv-house-serv","Protective-serv")
+                                           )
+                  )
 
-# Break up the data:
-n <- nrow(income)
-train <- sample(n, size = .8*n)
-test  <- setdiff(1:n, train)
-income.train <- income[train,]
-income.test  <- income[test, ]
+
+######################
+# tidymodels time!
+######################
+income_split <- initial_split(income, prop = 0.8)
+income_train <- training(income_split)
+income_test  <- testing(income_split)
+
+
+
+
+#####################
+# logistic regression
+#####################
+print('Starting LOGIT')
+# set up the task and the engine
+tune_logit_spec <- logistic_reg(
+  penalty = tune(), # tuning parameter
+  mixture = 1       # 1 = lasso, 0 = ridge
+) %>% 
+  set_engine("glmnet") %>%
+  set_mode("classification")
+
+# define a grid over which to try different values of the regularization parameter lambda
+lambda_grid <- grid_regular(penalty(), levels = 50)
+
+# 3-fold cross-validation
+rec_folds <- vfold_cv(income_train, v = 3)
+
+# Workflow
+rec_wf <- workflow() %>%
+  add_model(tune_logit_spec) %>%
+  add_formula(high.earner ~ education + marital.status + race + workclass + occupation + relationship + sex + age + capital.gain + capital.loss + hours)
+
+# Tuning results
+rec_res <- rec_wf %>%
+  tune_grid(
+    resamples = rec_folds,
+    grid = lambda_grid
+  )
+
+# what is the best value of lambda?
+top_acc  <- show_best(rec_res, metric = "accuracy")
+best_acc <- select_best(rec_res, metric = "accuracy")
+final_logit_lasso <- finalize_workflow(rec_wf,
+                                 best_acc
+                                )
+print('*********** LOGISTIC REGRESSION **************')
+logit_test <- last_fit(final_logit_lasso,income_split) %>%
+         collect_metrics()
+
+logit_test %>% print(n = 1)
+top_acc %>% print(n = 1)
+
+# combine results into a nice tibble (for later use)
+logit_ans <- top_acc %>% slice(1)
+logit_ans %<>% left_join(logit_test %>% slice(1),by=c(".metric",".estimator")) %>%
+               mutate(alg = "logit") %>% select(-starts_with(".config"))
+
+
+asdfafd
+
+#####################
+# tree model
+#####################
+print('Starting TREE')
+# set up the task and the engine
+tune_tree_spec <- decision_tree(
+  min_n = tune(), # tuning parameter
+  tree_depth = tune(), # tuning parameter
+  cost_complexity = tune(), # tuning parameter
+) %>% 
+  set_engine("rpart") %>%
+  set_mode("classification")
+
+# define a set over which to try different values of the regularization parameter (complexity, depth, etc.)
+tree_parm_df1 <- tibble(cost_complexity = seq(.001,.2,by=.05))
+tree_parm_df2 <- tibble(min_n = seq(10,100,by=10))
+tree_parm_df3 <- tibble(tree_depth = seq(5,20,by=5))
+tree_parm_df  <- full_join(tree_parm_df1,tree_parm_df2,by=character()) %>% full_join(.,tree_parm_df3,by=character())
+
+# YOU FILL IN THE REST
+
+
+
+
+#####################
+# neural net
+#####################
+print('Starting NNET')
+# set up the task and the engine
+tune_nnet_spec <- mlp(
+  hidden_units = tune(), # tuning parameter
+  penalty = tune()
+) %>% 
+  set_engine("nnet") %>%
+  set_mode("classification")
+
+# define a set over which to try different values of the regularization parameter (number of neighbors)
+nnet_parm_df1 <- tibble(hidden_units = seq(1,10))
+lambda_grid   <- grid_regular(penalty(), levels = 10)
+nnet_parm_df  <- full_join(nnet_parm_df1,lambda_grid,by=character())
+
+# YOU FILL IN THE REST
+
+
+
+
+#####################
+# knn
+#####################
+print('Starting KNN')
+# set up the task and the engine
+tune_knn_spec <- nearest_neighbor(
+  neighbors = tune() # tuning parameter
+) %>% 
+  set_engine("kknn") %>%
+  set_mode("classification")
+
+# define a set over which to try different values of the regularization parameter (number of neighbors)
+knn_parm_df <- tibble(neighbors = seq(1,30))
+
+# YOU FILL IN THE REST
+
+
+
+#####################
+# SVM
+#####################
+print('Starting SVM')
+# set up the task and the engine
+tune_svm_spec <- svm_rbf(
+  cost = tune(), 
+  rbf_sigma = tune()
+) %>% 
+  set_engine("kernlab") %>%
+  set_mode("classification")
+
+# define a set over which to try different values of the regularization parameter (number of neighbors)
+svm_parm_df1 <- tibble(cost      = c(2^(-2),2^(-1),2^0,2^1,2^2,2^10))
+svm_parm_df2 <- tibble(rbf_sigma = c(2^(-2),2^(-1),2^0,2^1,2^2,2^10))
+svm_parm_df  <- full_join(svm_parm_df1,svm_parm_df2,by=character())
+
+# YOU FILL IN THE REST
+
+
+
+
+
+
+
+
+#####################
+# combine answers
+#####################
+all_ans <- bind_rows(logit_ans,tree_ans,nnet_ans,knn_ans,svm_ans)
+datasummary_df(all_ans %>% select(-.metric,-.estimator,-mean,-n,-std_err),output="markdown") %>% print
 
 
